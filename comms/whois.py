bfbd9c03b1e0994d5c9b5f0dd14c622f8163b6c4
Zen <wolfskyzen@gmail.com>
2017-04-01 23:47:03 -0700

    Fixed whois to only work on private messages
    = Fixed whois to only work on private messages
    = Also setup whois to filter for command type messages only

--- comms/whois.py
+++ comms/whois.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
+from telegram import Chat
 from telegram.ext import CommandHandler
+from telegram.ext import Filters
 
 def setup_handler(dispatcher):
-    handler = CommandHandler('whois', whois)
+    handler = CommandHandler('whois', whois, Filters.command)
     dispatcher.add_handler(handler);
 
 def whois(bot, update):
+    if update.message.chat.type != Chat.PRIVATE:
+        return
     message = "Hello {0}, I am {1}, the Telegram communication bot for VancouFur staff!".format(update.message.from_user.first_name, bot.first_name)
     bot.sendMessage(update.message.chat_id, message)        
