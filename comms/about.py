04fee8963b4b6fbdb7aab798b3f537f0345ab6b4
Zen <wolfskyzen@gmail.com>
2017-08-03 20:19:37 -0700

    Added local git version to about command
    Fixes #7
    The about command now stores the local git version, if possible, and
    adds it to the about page

--- comms/about.py
+++ comms/about.py
@@ -2,14 +2,33 @@
 from telegram import Chat
 from telegram.ext import CommandHandler
 from telegram.ext import Filters
+import os
+import subprocess
+import sys
+
+git_version = None
 
 def handle_about(bot, update):
-    message = """VancouFur Staff Communications Bot
-Version 2.0
-Created by Zen using the Python Telegram Bot API.
-https://github.com/wolfskyzen/vfcommsbotpy"""
+    message = "VancouFur Staff Communications Bot" + os.linesep
+    global git_version
+    if git_version is not None:
+        message = message + "Version 2.0 ({0})".format(git_version) + os.linesep
+    else:
+        message = message + "Version 2.0" + os.linesep
+    message = message + "Created by Zen using the Python Telegram Bot API." + os.linesep
+    message = message + "https://github.com/wolfskyzen/vfcommsbotpy"
     bot.sendMessage(chat_id=update.message.chat_id, text=message, reply_to_message_id=update.message.message_id)
-
+    
+def setup():
+    try:
+        global git_version
+        git_version = subprocess.check_output("git log --pretty=format:""%h"" -1")
+        git_version = git_version.decode("utf-8")
+    except:
+        print("Unable to get current local git revision")
+        print(sys.exc_info())
+    
 def setup_handler(dispatcher):
+    setup()
     handler = CommandHandler('about', handle_about, Filters.command)
     dispatcher.add_handler(handler);
